347b016e16f7accb32c8739845f766cadf7634f8
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-02-24 08:38:22 +0000

    Dated reports

--- axe.py
+++ axe.py
@@ -1,12 +1,17 @@
 import csv
+from datetime import datetime
 from playwright.sync_api import sync_playwright
 from axe_core_python.sync_playwright import Axe
 from tqdm import tqdm
 
 axe = Axe()
 
-# Open the CSV file for writing
-with open('results.csv', mode='w', newline='') as file:
+# Get the current date and format it as dd-mm-yy
+now = datetime.now()
+date_string = now.strftime("%d-%m-%y")
+
+# Open the CSV file for writing with the new name
+with open(f'axe-report-{date_string}.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["URL", "Violations"])
 
