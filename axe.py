97c217d35a44c698d0d2dfc5994fb7f981f19974
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-02-24 08:02:28 +0000

    Formatting

--- axe.py
+++ axe.py
@@ -9,29 +9,29 @@ import datetime
 from tqdm import tqdm
 
 def scan_url(url):
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-driver = driver_factory.create_driver(chrome_options=chrome_options)
-driver.get(url)
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = driver_factory.create_driver(chrome_options=chrome_options)
+    driver.get(url)
 
-results = AxeResults()
-check_settings = CheckSettings()
-html_cs = HTMLCodeSnippet(driver.page_source)
-results = html_cs.run_rules(check_settings)
+    results = AxeResults()
+    check_settings = CheckSettings()
+    html_cs = HTMLCodeSnippet(driver.page_source)
+    results = html_cs.run_rules(check_settings)
 
-issues = ', '.join([issue["description"] for issue in results.violations])
-driver.quit()
-return issues
+    issues = ', '.join([issue["description"] for issue in results.violations])
+    driver.quit()
+    return issues
 
 with open('urls.txt', 'r') as file:
-urls = file.readlines()
+    urls = file.readlines()
 
 output_filename = f'axe-report-{datetime.datetime.now().strftime("%d-%m-%Y")}.csv'
 with open(output_filename, 'w', newline='') as csvfile:
-fieldnames = ['url', 'issues']
-writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-writer.writeheader()
+    fieldnames = ['url', 'issues']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
 
-for url in tqdm(urls):
-    issues = scan_url(url.strip())
-    writer.writerow({'url': url.strip(), 'issues': issues})
+    for url in tqdm(urls):
+        issues = scan_url(url.strip())
+        writer.writerow({'url': url.strip(), 'issues': issues})
